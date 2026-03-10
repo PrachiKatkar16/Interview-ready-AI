72a65db14bc721ba863cbff074453cf90a9ca970
PrachiKatkar16 <prachikatkar116@gmail.com>
2026-03-10 15:50:31 +0530

    changes made

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,19 +13,11 @@ const allowedOrigins = [
 ]
 
 app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true)
-
-    if(allowedOrigins.includes(origin)){
-      return callback(null, true)
-    } else {
-      return callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
-  methods: ["GET","POST","PUT","DELETE"],
-  allowedHeaders: ["Content-Type","Authorization"]
+  origin: allowedOrigins,
+  credentials: true
 }))
+
+app.options('*', cors())
 
 app.use(express.json())
 app.use(cookieParser())
